ab975c4912fb1d1902a5030d48eeb0c884b9a482
michalbanas <michal.banas@kiwi.com>
2024-02-15 09:15:24 +0100

    fix: base url

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: [["list"], ["html", { open: "on-failure" }]],
   use: {
-    baseURL: "http://localhost:3000/",
+    baseURL: process.env.BASE_URL ?? "http://localhost:3000/",
     testIdAttribute: "data-test",
     trace: "on-first-retry",
   },
