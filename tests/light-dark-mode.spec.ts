45a46ec6fb22769dc29f52c198e54ec57bb3a6b2
Michal Banas <banas.michal@icloud.com>
2024-02-15 15:52:47 +0100

    test: fixing light-dark mode (#14)
    * test: fixing light-dark mode
    * test: run toogle mode test only on desktop
    * test: skip visual testing tmp

--- tests/light-dark-mode.spec.ts
+++ tests/light-dark-mode.spec.ts
@@ -1,7 +1,8 @@
 import { expect } from "@playwright/test"
 import { test } from "./fixtures"
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({ page, isMobile }) => {
+  test.fixme(isMobile, "This test is not yet supported on mobile")
   await page.goto("/")
 })
 
@@ -13,36 +14,36 @@ test("should check light/dark mode functionality", async ({ page }) => {
 
   await test.step("Check the default state (dark)", async () => {
     await html.waitFor({ state: "visible" })
-    await expect(darkMode).toBeVisible()
     await expect(html).toHaveClass("dark")
+    await darkMode.waitFor({ state: "visible" })
   })
 
   await test.step("Check the light mode", async () => {
     await themeToogle.click()
     await html.waitFor({ state: "visible" })
-    await expect(lightMode).toBeVisible()
     await expect(html).toHaveClass("light")
+    await expect(lightMode).toBeVisible()
   })
 
   await test.step("Check that theme persists when clicking on a different page", async () => {
-    const article_first = page.locator("h2").first()
-    await article_first.click()
+    const header_first = page.locator("h2").first()
+    await header_first.click()
 
     const h1 = page.locator("h1")
     await h1.waitFor({ state: "visible" })
 
-    await expect(lightMode).toBeVisible()
     await expect(html).toHaveClass("light")
+    await expect(lightMode).toBeVisible()
   })
 
   await test.step("Change theme to dark mode and check that it persists when refreshing the page", async () => {
     await themeToogle.click()
-    await expect(darkMode).toBeVisible()
     await expect(html).toHaveClass("dark")
+    await expect(darkMode).toBeVisible()
 
     await page.reload()
     await html.waitFor({ state: "visible" })
-    await expect(darkMode).toBeVisible()
     await expect(html).toHaveClass("dark")
+    await expect(darkMode).toBeVisible()
   })
 })
