9c07608a69d57659386ea2da714d4c7add938443
michalbanas <banas.michal@icloud.com>
2024-03-01 08:27:04 +0100

    fix(reporter): path and folder

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,9 +15,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: 2,
   workers: process.env.CI ? 4 : "50%",
-  reporter: process.env.CI
-    ? [["json", { outputFile: "tests/results/integration-tests/results.json" }]]
-    : [["list"], ["html", { open: "on-failure" }]],
+  reporter: process.env.CI ? [["json", { outputFile: "results.json" }]] : [["list"], ["html", { open: "on-failure" }]],
   use: {
     baseURL: "http://localhost:8080/",
     testIdAttribute: "data-test",
