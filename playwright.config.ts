58910855a8ca57a0619c49265e9a47ebbe9c4530
michalbanas <banas.michal@icloud.com>
2024-03-01 08:36:13 +0100

    test: results

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,21 +1,12 @@
 import { defineConfig, devices } from "@playwright/test"
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: "./tests/integration-tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: 2,
   workers: process.env.CI ? 4 : "50%",
-  reporter: process.env.CI ? [["json", { outputFile: "results.json" }]] : [["list"], ["html", { open: "on-failure" }]],
+  reporter: process.env.CI ? [["json", { outputFile: "./results.json" }]] : [["list"], ["html", { open: "on-failure" }]],
   use: {
     baseURL: "http://localhost:8080/",
     testIdAttribute: "data-test",
